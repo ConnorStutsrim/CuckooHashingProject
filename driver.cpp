c1f1d648cc831d29cceddd0ee786990f6c066baa
ConnorStutsrim <connorsttsrm@gmail.com>
2015-04-23 23:22:59 -0400

    Created simple input driver
    Implemented file input and created framework for file output
    Implemented hash table building
    Implemented hash operation protocol
    
    Accepts input of the form
    <number of tables> <table size> <table size>...
    <operation> <value>
    <operation> <value>
    <operation> <value>
    <operation> <value>

--- driver.cpp
+++ driver.cpp
@@ -1,25 +1,65 @@
 #include "hash.h"
 
 #include <iostream>
+#include <fstream>
 #include <vector>
+#include <string>
 
-using namespace std;
+int main(int argc, char* argv[]) {
+	if (argc != 3) {
+		std::cout << "Error: incorrect syntax" << std::endl;
+		std::cout << "cuckoo <input> <output>" << std::endl;
+		exit(0);
+	}
+	
+	//Setting up file I/O
+	std::ifstream input;
+	input.open(argv[1]);
+	std::ofstream output;
+	output.open(argv[2]);
+	if (!input || !output) {
+		std::cout << "Error: One of the specified files does not exist" << std::endl;
+		exit(0);
+	}
+	
+	//Building the hash table
+	std::vector<int> tableSizes;
+	std::string tempString;
+	input >> tempString;
+	int tableNumber = std::stoi(tempString);
+	for (int i = 0; i < tableNumber; i++) {
+		input >> tempString;
+		int size = std::stoi(tempString);
+		tableSizes.push_back(size);
+	}
+	hash cuckoo(tableSizes);
+	std::cout << "Hash table built" << std::endl;
 
-int main(){
-	vector<int> tableSizes;
-	tableSizes.push_back(10);
-	tableSizes.push_back(12);
-	tableSizes.push_back(14);
-	hash h(tableSizes);
-	h.insert(120);
-	h.insert(240);
-	h.insert(360);
-	cout << h.search(120);
-	cout << h.search(240);
-	cout << h.search(360);
-	h.remove(120);
-	cout << h.search(120);
-	cout << endl;
-	h.print();
-	return 0;
+	//Executing operations
+	while(input >> tempString) {
+		std::cout << tempString << std::endl;
+		if(tempString == "insert") {
+			input >> tempString;
+			cuckoo.insert(std::stoi(tempString));
+		}
+		else if (tempString == "search") {
+			input >> tempString;
+			cuckoo.search(std::stoi(tempString));
+		}
+		else if (tempString == "remove") {
+			input >> tempString;
+			cuckoo.remove(std::stoi(tempString));
+		}
+		else if (tempString == "print") {
+			cuckoo.print();
+		}
+		else {
+			std::cout << "Error: improper operation list" << std::endl;
+			std::cout << "insert <value>" << std::endl;
+			std::cout << "search <value>" << std::endl;
+			std::cout << "remove <value>" << std::endl;
+			std::cout << "print" << std::endl;
+		}
+	}
+return 0;
 }
